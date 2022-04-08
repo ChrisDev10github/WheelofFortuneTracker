afa40ba794dc5a680aebc0b6733e13ddc0ae4088
Christopher Ng <CNg@dev-10.com>
2022-04-08 16:42:28 -0500

    msg

--- Wheel of Fortune.py
+++ Wheel of Fortune.py
@@ -1,1 +1,9 @@
 #Wheel of Fortune
+import random
+
+player1amount = 0
+player2amount = 0
+player3amount = 0
+
+word=[]
+wheel = [100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,'Bankrupt','Lose Turn']
